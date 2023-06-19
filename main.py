dc98d67cc7fd89cb78c03c5ebea53b3548290add
Maria Fernanda Gonzalez <mariagr@vedur.is>
2023-06-19 10:38:16 +0000

    trying to add file

---
+++ main.py
@@ -0,0 +1,7 @@
+#Import multiplication from your library
+from medium_multiply.multiplication import Multiplication
+
+#instantiate a multiplication object
+multiplication = Multiplication(2)
+
+print(multiplication.multiply(5))
